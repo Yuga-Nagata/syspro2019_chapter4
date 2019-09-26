629efb686f709f62d67cefef9abb0a7dea429278
Yuga-Nagata <s16140@tokyo.kosen-ac.jp>
2019-09-26 06:10:07 +0100

    サーボモータを回す関数の実装

--- servo.py
+++ servo.py
@@ -6,20 +6,17 @@ GPIO.setup(2, GPIO.OUT)
 servo = GPIO.PWM(2, 50)
 servo.start(0.0)
 
-bottom = 2.5
-middle = 7.2
-top = 12.0
+def setservo(arg):
+	duty = ( (2.4-0.5)*(arg+90)/180 + 0.5 ) / 20 * 100
+	servo.ChangeDutyCycle(duty)
 
-for i in range(5):
-	servo.ChangeDutyCycle(bottom)
-	time.sleep(1.0)
+setservo(-90)
+time.sleep(1.0)
 
-	servo.ChangeDutyCycle(middle)
-	time.sleep(1.0)
+setservo(0)
+time.sleep(1.0)
 
-	servo.ChangeDutyCycle(top)
-	time.sleep(1.0)
+setservo(90)
+time.sleep(1.0)
 
-
-
-
+GPIO.cleanup()
